fb896c81f5030adc4e802881a225e3f7035ce428
Edson-Menezes <menezes.edjr@gmail.com>
2020-02-20 16:30:17 -0300

    Revert "petshop express v1.0"
    This reverts commit 51bed49d8090b2d54d3fbcd3810b8307367cbd69.

--- routes/pet.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const petController =  require("../controller/petController")
-
-router.get("/", petController.index);
-router.get("/add/:nome", petController.add);
-router.get("/buscar/:nome", petController.buscar);
-
-module.exports = router;
